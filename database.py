267c15f709e0415e3a373dbaddf3ca6c9722d716
wisdompathways0018-create <wisdompathways0018@gmail.com>
2026-06-29 22:45:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,55 @@
-database 
+import json
+import os
+
+DATABASE_FILE = "database.json"
+
+
+def load_database():
+    if not os.path.exists(DATABASE_FILE):
+        return {}
+
+    try:
+        with open(DATABASE_FILE, "r") as f:
+            return json.load(f)
+    except Exception:
+        return {}
+
+
+def save_database(data):
+    with open(DATABASE_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+database = load_database()
+
+
+def get_guild(guild_id):
+
+    guild_id = str(guild_id)
+
+    if guild_id not in database:
+
+        database[guild_id] = {
+
+            "alliance_name": None,
+            "timezone": "UTC",
+
+            "reminder_channel": None,
+            "ping_role": None,
+
+            "frost": [],
+            "ib": [],
+            "supremacy": [],
+            "lords": [],
+            "relic": [],
+            "malena": [],
+            "custom_events": []
+        }
+
+        save_database(database)
+
+    return database[guild_id]
+
+
+def save():
+    save_database(database)
