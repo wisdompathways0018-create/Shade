5287166a06207f6211c0e7b9be83912a54d06b23
wisdompathways0018-create <wisdompathways0018@gmail.com>
2026-06-29 22:46:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,9 @@
-# ==========================================
-# Shade Configuration
-# ==========================================
-
-server_config = {}
-
-
-def create_server(server_id):
-    if server_id not in server_config:
-        server_config[server_id] = {
-            "alliance_name": None,
-            "timezone": "UTC",
-            "reminder_channel": None,
-            "ping_role": "@everyone",
-
-            "frost": [],
-            "supremacy": {},
-            "relic": {},
-            "lords": {},
-            "malena": {},
-            "custom_events": []
-        }
+from database import get_guild, save
 
 
 def get_server(server_id):
-    create_server(server_id)
-    return server_config[server_id]
+    return get_guild(server_id)
+
+
+def save_server():
+    save()
